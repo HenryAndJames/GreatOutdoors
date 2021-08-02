54471cb5e569315559f59c4ef4b74d484ff2ddea
HenryAndJames <88346627+HenryAndJames@users.noreply.github.com>
2021-08-02 14:30:30 -0700

    Added field to DeliveryAddress

--- Cogent-Great-Outdoors/src/main/java/cogent/go/entities/DeliveryAddress.java
+++ Cogent-Great-Outdoors/src/main/java/cogent/go/entities/DeliveryAddress.java
@@ -1,8 +1,24 @@
 package cogent.go.entities;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.Size;
 
 @Entity
+@Table(name = "delivery_address_table", schema = "greatoutdoors")
 public class DeliveryAddress {
-
+	@Id
+	@Column(name = "addressId")
+	private int id;
+	@Size(max = 60)
+	private String addressLine1;
+	@Size(max = 60)
+	private String addressLine2;
+	@Size(max = 30)
+	private String state;
+	private int pincode;
+	
+	
 }
