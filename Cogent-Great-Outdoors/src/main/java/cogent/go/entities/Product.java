b830f5ab9d9840be18449bb3d8632cce93d42c63
HenryAndJames <88346627+HenryAndJames@users.noreply.github.com>
2021-08-02 14:48:47 -0700

    Added getters and setters to Product

--- Cogent-Great-Outdoors/src/main/java/cogent/go/entities/Product.java
+++ Cogent-Great-Outdoors/src/main/java/cogent/go/entities/Product.java
@@ -15,4 +15,48 @@ public class Product {
 	private int price;
 	@Size(max = 20)
 	private String category;
+	public Product() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public Product(int id, @Size(max = 30) String name, @Size(max = 200) String description, int price,
+			@Size(max = 20) String category) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.description = description;
+		this.price = price;
+		this.category = category;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public String getCategory() {
+		return category;
+	}
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	
 }
