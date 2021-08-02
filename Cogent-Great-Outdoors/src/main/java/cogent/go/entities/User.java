a21adfec07bf58aabbc12c110228e5fa26fb3219
HenryAndJames <88346627+HenryAndJames@users.noreply.github.com>
2021-08-02 13:47:02 -0700

    added two fields to entity

--- Cogent-Great-Outdoors/src/main/java/cogent/go/entities/User.java
+++ Cogent-Great-Outdoors/src/main/java/cogent/go/entities/User.java
@@ -1,8 +1,14 @@
 package cogent.go.entities;
 
 import javax.persistence.Entity;
+import javax.persistence.Id;
 
 @Entity
 public class User {
+	
+	@Id
+	private int id;
+	
+	private String firstName;
 
 }
