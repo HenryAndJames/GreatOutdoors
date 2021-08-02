2fb8836062e98ed9e299dd2b1962d103175e113d
HenryAndJames <88346627+HenryAndJames@users.noreply.github.com>
2021-08-02 14:32:16 -0700

    Added setter and getter to DeliveryAddress

--- Cogent-Great-Outdoors/src/main/java/cogent/go/entities/DeliveryAddress.java
+++ Cogent-Great-Outdoors/src/main/java/cogent/go/entities/DeliveryAddress.java
@@ -20,5 +20,59 @@ public class DeliveryAddress {
 	private String state;
 	private int pincode;
 	
+	public DeliveryAddress() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public DeliveryAddress(int id, @Size(max = 60) String addressLine1, @Size(max = 60) String addressLine2,
+			@Size(max = 30) String state, int pincode) {
+		super();
+		this.id = id;
+		this.addressLine1 = addressLine1;
+		this.addressLine2 = addressLine2;
+		this.state = state;
+		this.pincode = pincode;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getAddressLine1() {
+		return addressLine1;
+	}
+
+	public void setAddressLine1(String addressLine1) {
+		this.addressLine1 = addressLine1;
+	}
+
+	public String getAddressLine2() {
+		return addressLine2;
+	}
+
+	public void setAddressLine2(String addressLine2) {
+		this.addressLine2 = addressLine2;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public int getPincode() {
+		return pincode;
+	}
+
+	public void setPincode(int pincode) {
+		this.pincode = pincode;
+	}
 	
 }
